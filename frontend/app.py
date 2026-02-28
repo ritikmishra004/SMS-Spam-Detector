0c7e3b2670d89db9d1c33efc1a9edf8be2c3c3a4
ritikmishra004 <Ritikmishra1004@gmail.com>
2026-02-28 12:44:55 +0530

    Initial commit - SMS Spam Detector Production Project

--- frontend/app.py
+++ frontend/app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pickle
 import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('punkt_tab')
 from nltk.corpus import stopwords
 import string
 from nltk.stem.porter import PorterStemmer
